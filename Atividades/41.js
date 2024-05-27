c297d6d2e4b134355766222b8975ec7fa9a3fde8
IsraelDev560 <devisrael560@gmail.com>
2024-05-27 12:09:34 -0300

    Algoritmo: 'Objeto pessoa'

---
+++ Atividades/41.js
@@ -0,0 +1,11 @@
+// 41. Dado o objeto pessoa com propriedades nome e idade, acesse e imprima o valor de
+// idade. Adicione uma nova propriedade chamada email ao objeto pessoa que já possui
+// nome e idade.
+
+let pessoa = {
+    nome: "Israel",
+    idade: 19,
+};
+console.log(pessoa.idade);
+pessoa.email = "israelcardeal@gmail.com";
+console.log(pessoa);
