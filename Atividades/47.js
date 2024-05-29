89a060ac775718eaece56f5585d8c8633e5224a6
IsraelDev560 <devisrael560@gmail.com>
2024-05-29 12:43:05 -0300

    Algoritmo: 'Transformando em maiusculas'

--- Atividades/47.js
+++ Atividades/47.js
@@ -1,16 +1,16 @@
 // 47. Crie uma função que transforme um objeto de entrada aplicando uma função
 // fornecida a cada uma das propriedades do objeto, retornando um novo objeto com os
 // resultados.
-function transforme(objeto) {
+function transforme(pessoas) {
    let transformado = {};
-   objeto.forEach(atributo => {
-         transformado[atributo.nome] = atributo.nome.toUpperCase();
+   pessoas.forEach(pessoa => {
+         transformado[pessoa.nome] = pessoa.nome.toUpperCase();
    });  
    return transformado;
 }
-let objeto = [
+let pessoas = [
    {nome: "Carlos", idade: 22},
    {nome: "José", idade: 40},
    {nome: "Paulo", idade: 25}
 ];
-console.log(transforme(objeto));
+console.log(transforme(pessoas));
