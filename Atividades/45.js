07024d117bf57fd0e31995bc1489a4287a573d20
IsraelDev560 <devisrael560@gmail.com>
2024-06-06 12:06:00 -0300

    Algoritmo: 'Contando elementos de um objeto'

--- Atividades/45.js
+++ Atividades/45.js
@@ -1,18 +1,20 @@
-// 45. Dado um array de strings, crie um objeto onde cada string é uma chave, e seu valor é
-// o número de vezes que a string aparece no array.
-
-function contandoObjetos(arrayDeStrings) {
-    let obj = {};
-    arrayDeStrings.forEach((element) => {
-        if(obj[element]){
-            obj[element]++;
-        }
-        else{
-            obj[element] = 1;
-        }
-    });
-    return obj;
+function funcao45() {
+    // 45. Dado um array de strings, crie um objeto onde cada string é uma chave, e seu valor é
+    // o número de vezes que a string aparece no array.
+    function contandoObjetos(arrayDeStrings) {
+        let obj = {};
+        arrayDeStrings.forEach((element) => {
+            if (obj[element]) {
+                obj[element]++;
+            }
+            else {
+                obj[element] = 1;
+            }
+        });
+        return obj;
+    }
+    let arrayDeStrings = ["maçã", "banana", "laranja", "uva", "manga", "banana", "maçã"];
+    console.log(contandoObjetos(arrayDeStrings));
 }
-let arrayDeStrings = ["maçã", "banana", "laranja", "uva", "manga", "banana", "maçã"];
-console.log(contandoObjetos(arrayDeStrings));
+module.exports.funcao45 = funcao45;
 
