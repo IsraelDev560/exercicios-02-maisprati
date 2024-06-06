1411a8ed28bfabbf740d6388fc614586cca2e718
IsraelDev560 <devisrael560@gmail.com>
2024-06-06 10:46:30 -0300

    Algoritmo: 'Ler 7 nomes e guardar em um vetor'

--- Atividades/14.js
+++ Atividades/14.js
@@ -1,14 +1,17 @@
-// 14. Faça um programa que leia 7 nomes de pessoas e guarde-os em um vetor. No final,
-// mostre uma listagem com todos os nomes informados, na ordem inversa daquela em
-// que eles foram informados.
+function funcao14() {
+    // 14. Faça um programa que leia 7 nomes de pessoas e guarde-os em um vetor. No final,
+    // mostre uma listagem com todos os nomes informados, na ordem inversa daquela em
+    // que eles foram informados.
 
-const vetor = [];
+    const vetor = [];
 
-const prompt = require('prompt-sync')();
+    const prompt = require('prompt-sync')();
 
-for (i = 1; i <= 7; i++) {
-    let nomes = prompt("Digite o nome da pessoa: ");
-    vetor.push(nomes)
-    vetor.reverse(nomes)
+    for (i = 1; i <= 7; i++) {
+        let nomes = prompt("Digite o nome da pessoa: ");
+        vetor.push(nomes);
+        vetor.reverse(nomes);
+    }
+    console.log(vetor);
 }
-console.log(vetor);
+module.exports.funcao14 = funcao14;
