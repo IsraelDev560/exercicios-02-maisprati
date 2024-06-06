b1616c4def58a6eed92cfcbd96294ee9541207be
IsraelDev560 <devisrael560@gmail.com>
2024-06-06 10:28:41 -0300

    Algoritmo: 'Ler o Salário e Sexo dos funcionários'

--- Atividades/09.js
+++ Atividades/09.js
@@ -1,3 +1,4 @@
+function funcao09(){
 // 9. Desenvolva um aplicativo que leia o salário e o sexo de vários funcionários. No final,
 // mostre o total de salário pago aos homens e o total pago às mulheres. O programa vai
 // perguntar ao usuário se ele quer continuar ou não sempre que ler os dados de um
@@ -10,7 +11,7 @@ const funcionarios = [
     { nome: 'Carla', sexo: 'Feminino', salario: 1810 }
 ]
 
-const salariosFuncionarios = {}
+const salariosFuncionarios = {};
 
 function lerSalario(funcionarios) {
     while (true) {
@@ -40,4 +41,6 @@ function lerSalario(funcionarios) {
     });
     console.log("Salarios por gênero:", salariosFuncionarios);
 }
-lerSalario(funcionarios)
+lerSalario(funcionarios);
+}
+module.exports.funcao09 = funcao09;
