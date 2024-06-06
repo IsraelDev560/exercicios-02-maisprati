8b57d62baaa2a0f17f57fb8bbc5eb52380f3d560
IsraelDev560 <devisrael560@gmail.com>
2024-06-06 12:09:16 -0300

    Algoritmo: 'Juntando inventarios'

--- Atividades/48.js
+++ Atividades/48.js
@@ -1,4 +1,5 @@
 function funcao48() {
+    
     // 48. Você recebe dois objetos que representam o inventário de duas lojas diferentes:
     // inventarioLojaA e inventarioLojaB. Cada chave é um item, e o valor é a quantidade desse
     // item em estoque. Escreva uma função que combine os inventários em um único objeto.
